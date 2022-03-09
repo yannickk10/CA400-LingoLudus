3255464a522f9ece2e09ff9dcb475f0e1242a2bc
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-03-09 17:46:24 +0000

    adding spanish vocab list

--- src/enemy_spawner.py
+++ src/enemy_spawner.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from enemy import Enemy
 from spanish_vocab import spanish_vehicles
+import random
 
 
 class EnemySpawner:
@@ -16,3 +17,10 @@ class EnemySpawner:
                 self.spawn_timer = 120
             else:
                 self.spawn_timer -= 1
+
+
+        def spawn_enemy(self):
+            #create and shuffle list
+            spanish_vehicles_lis = list(spanish_vehicles.items())
+            random.shuffle(spanish_vehicles_lis)
+            shuffled_spanish_vehicles = dict(spanish_vehicles_lis)
