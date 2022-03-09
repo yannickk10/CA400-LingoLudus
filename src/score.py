16f65630655ef5602667cb0b43644fd65fec12d0
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-09 19:48:54 +0000

    added update_score method

--- src/score.py
+++ src/score.py
@@ -10,3 +10,13 @@ class Score(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH - self.rect.width - 30
         self.rect.y = SCREEN_HEIGHT - self.rect.height - 30
+
+    
+
+    def update_score(self):
+        self.score += score
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.image = self.font.render("Score:" + str(self.score), False, (255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH - self.rect.width - 30
+        self.rect.y = SCREEN_HEIGHT - self.rect.height - 30
