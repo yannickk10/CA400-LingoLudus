3b4c6d74c1ca03d1ea02769b34a2e5ec307ef540
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-03-09 17:38:49 +0000

    updating enemy movement and spawn time

--- src/enemy_spawner.py
+++ src/enemy_spawner.py
@@ -8,3 +8,11 @@ class EnemySpawner:
         def __init__(self):
             self.enemy_group = pygame.sprite.Group()
             self.spawn_timer  = 120
+
+        def update(self):
+            self.enemy_group.update()
+            if self.spawn_timer == 0:
+                self.spawn_enemy()
+                self.spawn_timer = 120
+            else:
+                self.spawn_timer -= 1
