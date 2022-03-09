f59359b7cbb13ee6b8e56db50827cdad75033298
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-03-09 16:55:44 +0000

    making the bullet groups for the ships

--- src/ship.py
+++ src/ship.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from bullet import Bullet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -9,3 +10,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("Sprites/jet.png").convert()
         self.image.set_colorkey((255,255,255), pygame.RLEACCEL)
         self.rect = self.image.get_rect()
+
+        #creating the bullet group for the ships bullets
+        self.bullets = pygame.sprite.Group()
+        self.shoot_cooldown = 0
