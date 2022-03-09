c5e975acc438c7548a0d5841038e4b273a7e8f15
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-09 17:13:07 +0000

    create span vocab file containing sprites in dict

---
+++ src/spanish_vocab.py
@@ -0,0 +1,8 @@
+spanish_vehicles ={
+    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
+    "el coche": ("Sprites/Car.png", (0,0,0)),
+    "entrenar": ("Sprites/Train.png", (0,0,0)),
+    "apple": ("Sprites/blueberry.png", (0,0,0)),
+    "bluebery": ("Sprites/red_apple.png", (0,0,0)),
+    "grapes": ("Sprites/purple_grapes.png", (0,0,0))
+	}
