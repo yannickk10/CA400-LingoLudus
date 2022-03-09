17d0ea18ca3a3870d475e281eafb7c410da7845a
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-09 17:19:11 +0000

    Created Enemy Object

---
+++ src/enemy.py
@@ -0,0 +1,17 @@
+import pygame
+from settings import *
+import random
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, sprite, backing_colour):
+        super(Enemy, self).__init__()
+        self.image = pygame.image.load(sprite).convert_alpha()
+        self.image.set_colorkey(backing_colour, pygame.RLEACCEL)
+        self.rect = self.image.get_rect(center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(10, SCREEN_HEIGHT- 120),))
+
+        self.speed = random.randint(5,6)
+    
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
