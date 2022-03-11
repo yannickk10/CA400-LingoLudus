741a4e3320361e7c21526b9dbfc8ae03c4d083eb
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-11 11:44:32 +0000

    work on stopping spawn overlap add imposter score

--- src/enemy.py
+++ src/enemy.py
@@ -5,21 +5,35 @@ import random
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, sprite, backing_colour):
         super(Enemy, self).__init__()
-        self.image = pygame.image.load(sprite).convert_alpha()
+        self.image = pygame.image.load(sprite).convert()
         self.image.set_colorkey(backing_colour)
-        self.rect = self.image.get_rect(center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(10, SCREEN_HEIGHT- 120),))
-
-        self.speed = random.randint(5,6)
+        self.start_pos_divider = 6
+        self.rect, self.rect.x, self.rect.y = self.set_pos()
+        self.speed = random.randint(6,7)
 
         #Enenmy status
         self.health = 1
-        self.enemy_score = 50
+        self.enemy_score = 0
+        self.imposter_score = 100
         self.speed = random.randint(5,6)
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
+
+    def set_pos(self):
+
+        if self.start_pos_divider == 1:
+            self.rect = self.image.get_rect()
+            self.rect.y = SCREEN_HEIGHT / self.start_pos_divider
+            self.rect.x = SCREEN_WIDTH
+        else:
+            self.rect = self.image.get_rect()
+            self.rect.y = SCREEN_HEIGHT / self.start_pos_divider
+            self.rect.x = SCREEN_WIDTH
+        self.start_pos_divider -= 1
+        print(self.start_pos_divider)
+        
+        return self.rect, self.rect.x, self.rect.y
 
     def get_hit(self):
         self.health -= 1
