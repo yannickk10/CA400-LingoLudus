3eecd5c3eaebc778ce720e6392f906a394e5c7e5
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-11 11:45:22 +0000

    Added target to HUD

--- src/heads_up_display.py
+++ src/heads_up_display.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from score import Score
+from display_target import Target
 
 class HUD (pygame.sprite.Sprite):
     def __init__(self):
@@ -9,10 +10,16 @@ class HUD (pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y = SCREEN_HEIGHT - self.rect.height
 
-        #Add Score to health Bar
+        #Add Score to hud
         self.score_object = Score()
         self.player_score = pygame.sprite.Group()
         self.player_score.add(self.score_object)
+
+        # Add target name to hud
+        self.target = Target()
+        self.target_name = pygame.sprite.Group()
+        self.target_name.add(self.target)
+
 
     def update(self):
         self.player_score.update()
