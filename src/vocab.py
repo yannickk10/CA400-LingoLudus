494dbfd9dcfa25787a64c78ecabf5581eea9fcc4
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-15 03:56:30 +0000

    add french to vocab file

---
+++ src/vocab.py
@@ -0,0 +1,18 @@
+language = 'french'
+spanish_vehicles ={
+    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
+    "el coche": ("Sprites/Car.png", (0,0,0)),
+    "entrenar": ("Sprites/Train.png", (0,0,0)),
+    "apple": ("Sprites/blueberry.png", (0,0,0)),
+    "bluebery": ("Sprites/red_apple.png", (0,0,0)),
+    "grapes": ("Sprites/purple_grapes.png", (0,0,0))
+	}
+
+french_vehicles ={
+    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
+    "le car": ("Sprites/Car.png", (0,0,0)),
+    "le train": ("Sprites/Train.png", (0,0,0)),
+    "Pomme": ("Sprites/blueberry.png", (0,0,0)),
+    "myrtille": ("Sprites/red_apple.png", (0,0,0)),
+    "les raisins": ("Sprites/purple_grapes.png", (0,0,0))
+	}
