efe2b178784af9717127516d5075a316a4393969
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-16 23:54:08 +0000

    removd button code to be refactored

--- src/menu.py
+++ src/menu.py
@@ -1,54 +1,10 @@
 import pygame, select_language
 from settings import *
+from button import Button
 import sys
 
 
 def main_menu():
-
-	class Button:
-		def __init__(self, text, width, height, pos,elevation):
-			self.top_rectangle = pygame.Rect(pos, (width,height))
-			self.top_rectangle_color = '#475F77'
-
-			self.text_surf = gui_font.render(text,True,'#FFFFFF')
-			self.text_rect = self.text_surf.get_rect(center = self.top_rectangle.center)
-
-			self.bottom_rectangle = pygame.Rect(pos, (width,elevation))
-			self.bottom_rectangle_color = '#354B5E'
-
-			self.orig_elevation = elevation
-			self.elevation_copy = elevation
-			self.original_y_position = pos[1]
-
-			self.pressed = False
-
-		def draw(self):
-			self.top_rectangle.y = self.original_y_position - self.elevation_copy
-			self.text_rect.center = self.top_rectangle.center
-
-			self.bottom_rectangle.midtop = self.top_rectangle.midtop
-			self.bottom_rectangle.height = self.top_rectangle.height + self.elevation_copy
-
-			pygame.draw.rect(screen,self.bottom_rectangle_color, self.bottom_rectangle, border_radius=12)
-
-			pygame.draw.rect(screen,self.top_rectangle_color, self.top_rectangle, border_radius=12)
-			screen.blit(self.text_surf, self.text_rect)
-			self.if_pressed()
-
-		def if_pressed(self):
-			mouse_position = pygame.mouse.get_pos()
-			if self.top_rectangle.collidepoint(mouse_position):
-				self.top_rectangle_color = '#D74B4B'
-				if pygame.mouse.get_pressed()[0]:
-					self.elevation_copy = 0
-					self.pressed = True
-				else:
-					if self.pressed == True:
-						self.pressed = False
-						self.elevation_copy = self.orig_elevation
-			else:
-				self.top_rectangle_color = '#475F77'
-				self.elevation_copy = self.orig_elevation
 
 	class StartGameButton(Button):
 
@@ -116,10 +72,9 @@ def main_menu():
 	background_image = pygame.image.load("images/LiLu_Logo.png").convert()
 
 	# defining a font
-	gui_font = pygame.font.Font(None, 30)
 
-	start_game_button = StartGameButton('Start Game', 200, 40, (width/2-100,300), 6)
-	quit_game_button = QuitGameButton('Quit Game', 200, 40, (width/2-100,420), 6)
+	start_game_button = StartGameButton('Start Game', pygame.font.Font(None, 30), 200, 40, (width/2-100,300), 6, screen)
+	quit_game_button = QuitGameButton('Quit Game', pygame.font.Font(None, 30),  200, 40, (width/2-100,420), 6, screen)
 
 	gameLoop = True
 
--- src/select_language.py
+++ src/select_language.py
@@ -1,55 +1,9 @@
 import pygame
 import game_sel_menu
-import button as Button
+from button import *
 from settings import *
 
 def language_select():
-
-    class Button:
-        def __init__(self, text, width, height, pos,elevation):
-            self.top_rectangle = pygame.Rect(pos, (width,height))
-            self.top_rectangle_color = '#475F77'
-
-            self.text_surf = gui_font.render(text,True,'#FFFFFF')
-            self.text_rect = self.text_surf.get_rect(center = self.top_rectangle.center)
-
-            self.bottom_rectangle = pygame.Rect(pos, (width,elevation))
-            self.bottom_rectangle_color = '#354B5E'
-
-            self.orig_elevation = elevation
-            self.elevation_copy = elevation
-            self.original_y_position = pos[1]
-
-            self.pressed = False
-
-        def draw(self):
-            self.top_rectangle.y = self.original_y_position - self.elevation_copy
-            self.text_rect.center = self.top_rectangle.center
-
-            self.bottom_rectangle.midtop = self.top_rectangle.midtop
-            self.bottom_rectangle.height = self.top_rectangle.height + self.elevation_copy
-
-            pygame.draw.rect(screen,self.bottom_rectangle_color, self.bottom_rectangle, border_radius=12)
-
-            pygame.draw.rect(screen,self.top_rectangle_color, self.top_rectangle, border_radius=12)
-            screen.blit(self.text_surf, self.text_rect)
-            self.if_pressed()
-
-        def if_pressed(self):
-            mouse_position = pygame.mouse.get_pos()
-            if self.top_rectangle.collidepoint(mouse_position):
-                self.top_rectangle_color = '#D74B4B'
-                if pygame.mouse.get_pressed()[0]:
-                    self.elevation_copy = 0
-                    self.pressed = True
-                else:
-                    if self.pressed == True:
-                        print('click')
-                    self.pressed = False
-                    self.elevation_copy = self.orig_elevation
-            else:
-                self.top_rectangle_color = '#475F77'
-                self.elevation_copy = self.orig_elevation
 
     class SpanishButton(Button):
 
@@ -116,37 +70,6 @@ def language_select():
                 self.top_rectangle_color = '#475F77'
                 self.elevation_copy = self.orig_elevation
 
-    class GoBackButton(Button):
-
-        def draw(self):
-            self.top_rectangle.y = self.original_y_position - self.elevation_copy
-            self.text_rect.center = self.top_rectangle.center
-
-            self.bottom_rectangle.midtop = self.top_rectangle.midtop
-            self.bottom_rectangle.height = self.top_rectangle.height + self.elevation_copy
-
-            pygame.draw.rect(screen,self.bottom_rectangle_color, self.bottom_rectangle, border_radius=12)
-
-            pygame.draw.rect(screen,self.top_rectangle_color, self.top_rectangle, border_bottom_left_radius=12, border_top_left_radius=12)
-            screen.blit(self.text_surf, self.text_rect)
-            if self.if_pressed() == True:
-                return False
-
-        def if_pressed(self):
-            mouse_position = pygame.mouse.get_pos()
-            if self.top_rectangle.collidepoint(mouse_position):
-                self.top_rectangle_color = '#D74B4B'
-                if pygame.mouse.get_pressed()[0]:
-                    self.elevation_copy = 0
-                    self.pressed = True
-                else:
-                    if self.pressed == True:
-                        self.pressed = False
-                        self.elevation_copy = self.orig_elevation
-                        return True
-            else:
-                self.top_rectangle_color = '#475F77'
-                self.elevation_copy = self.orig_elevation
 
     # initializing the constructor
     pygame.init()
@@ -179,13 +102,12 @@ def language_select():
     background_image = pygame.image.load("images/LiLu_Logo.png").convert()
 
     # defining a font
-    gui_font = pygame.font.Font(None, 30)
 
     #create button object
 
-    select_french_button = FrenchButton('French', 200, 40, (width/2-100,300), 6)
-    select_spanish_button = SpanishButton('Spanish', 200, 40, (width/2-100,420), 6)
-    go_back_button = GoBackButton('Back', 80, 40, (10,670), 6)
+    select_french_button = FrenchButton('French', pygame.font.Font(None, 30), 200, 40, (width/2-100,300), 6, screen)
+    select_spanish_button = SpanishButton('Spanish', pygame.font.Font(None, 30), 200, 40, (width/2-100,420), 6, screen)
+    go_back_button = GoBackButton('Back',pygame.font.Font(None, 30), 80, 40, (10,670), 6, screen)
 
     gameLoop = True
 
@@ -221,8 +143,6 @@ def language_select():
 
         if go_back_button.draw() == False:
             gameLoop = False
-
-
 
         # updates the frames of the game
         pygame.display.update()
