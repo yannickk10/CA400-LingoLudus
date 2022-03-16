cd90956640512952b04f01759985fbc03f992c3d
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-16 23:53:02 +0000

    removed button code to be refactored

--- src/game_sel_menu.py
+++ src/game_sel_menu.py
@@ -2,56 +2,11 @@ import pygame
 import test as gt
 import sys
 from settings import *
+
 import pygame, sys
 
 
 def game_hub():
-
-	class Button:
-		def __init__(self, text, width, height, pos,elevation):
-			self.top_rectangle = pygame.Rect(pos, (width,height))
-			self.top_rectangle_color = '#475F77'
-
-			self.text_surf = gui_font.render(text,True,'#FFFFFF')
-			self.text_rect = self.text_surf.get_rect(center = self.top_rectangle.center)
-
-			self.bottom_rectangle = pygame.Rect(pos, (width,elevation))
-			self.bottom_rectangle_color = '#354B5E'
-			
-			self.orig_elevation = elevation
-			self.elevation_copy = elevation
-			self.original_y_position = pos[1]
-
-			self.pressed = False
-
-		def draw(self):
-			self.top_rectangle.y = self.original_y_position - self.elevation_copy
-			self.text_rect.center = self.top_rectangle.center
-			
-			self.bottom_rectangle.midtop = self.top_rectangle.midtop
-			self.bottom_rectangle.height = self.top_rectangle.height + self.elevation_copy
-
-			pygame.draw.rect(screen,self.bottom_rectangle_color, self.bottom_rectangle, border_radius=12)
-
-			pygame.draw.rect(screen,self.top_rectangle_color, self.top_rectangle, border_radius=12)
-			screen.blit(self.text_surf, self.text_rect)
-			self.if_pressed()
-
-		def if_pressed(self):
-			mouse_position = pygame.mouse.get_pos()
-			if self.top_rectangle.collidepoint(mouse_position):
-				self.top_rectangle_color = '#D74B4B'
-				if pygame.mouse.get_pressed()[0]:
-					self.elevation_copy = 0
-					self.pressed = True
-				else:
-					if self.pressed == True:
-						print('click')
-					self.pressed = False
-					self.elevation_copy = self.orig_elevation
-			else:
-				self.top_rectangle_color = '#475F77'
-				self.elevation_copy = self.orig_elevation
 
 	class WordInvadersButton(Button):
 		
@@ -67,38 +22,6 @@ def game_hub():
 						gt.space_invaders()
 					self.pressed = False
 					self.elevation_copy = self.orig_elevation
-			else:
-				self.top_rectangle_color = '#475F77'
-				self.elevation_copy = self.orig_elevation
-
-	class GoBackButton (Button):
-
-		def draw(self):
-			self.top_rectangle.y = self.original_y_position - self.elevation_copy
-			self.text_rect.center = self.top_rectangle.center
-			
-			self.bottom_rectangle.midtop = self.top_rectangle.midtop
-			self.bottom_rectangle.height = self.top_rectangle.height + self.elevation_copy
-
-			pygame.draw.rect(screen,self.bottom_rectangle_color, self.bottom_rectangle, border_radius=12)
-
-			pygame.draw.rect(screen,self.top_rectangle_color, self.top_rectangle, border_bottom_left_radius=12, border_top_left_radius=12)
-			screen.blit(self.text_surf, self.text_rect)
-			if self.if_pressed() == True:
-				return False
-
-		def if_pressed(self):
-			mouse_position = pygame.mouse.get_pos()
-			if self.top_rectangle.collidepoint(mouse_position):
-				self.top_rectangle_color = '#D74B4B'
-				if pygame.mouse.get_pressed()[0]:
-					self.elevation_copy = 0
-					self.pressed = True
-				else:
-					if self.pressed == True:
-						self.pressed = False
-						self.elevation_copy = self.orig_elevation
-						return True
 			else:
 				self.top_rectangle_color = '#475F77'
 				self.elevation_copy = self.orig_elevation
@@ -133,12 +56,8 @@ def game_hub():
 
 	background_image = pygame.image.load("images/LiLu_Logo.png").convert()
 
-	# defining a font
-	gui_font = pygame.font.Font(None, 30)
-
-
-	word_invaders_button = WordInvadersButton('Play Word Invaders', 200, 40, (width/2-100,300), 6)
-	go_back_button = GoBackButton('Back', 80, 40, (10,670), 6)
+	word_invaders_button = WordInvadersButton('Play Word Invaders', pygame.font.Font(None, 30), 200, 40, (width/2-100,300), 6, screen)
+	go_back_button = GoBackButton('Back', pygame.font.Font(None, 30), 80, 40, (10,670), 6, screen)
 	
 	gameLoop = True
 
