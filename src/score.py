f1582f9657a42fe2c3fa009bb666f3e29f23909d
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-17 00:06:30 +0000

    create puase loop which draws screen and button

--- src/score.py
+++ src/score.py
@@ -5,7 +5,7 @@ class Score(pygame.sprite.Sprite):
     def __init__(self):
         super(Score, self).__init__()
         self.score = 0
-        self.font = font = pygame.font.Font('freesansbold.ttf', 32)
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
         self.image = self.font.render("Score:" + str(self.score), False, (255,255,255))
         self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH - self.rect.width - 30
