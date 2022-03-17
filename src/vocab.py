1ecbde92e9085bb44e0de22aaa78d3b907dc0da4
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-03-17 00:27:25 +0000

    update spanish translations

--- src/vocab.py
+++ src/vocab.py
@@ -3,9 +3,9 @@ spanish_vehicles ={
     "projectile":  ("Sprites/projectile.gif", (0,0,0)),
     "el coche": ("Sprites/Car.png", (0,0,0)),
     "entrenar": ("Sprites/Train.png", (0,0,0)),
-    "apple": ("Sprites/blueberry.png", (0,0,0)),
-    "bluebery": ("Sprites/red_apple.png", (0,0,0)),
-    "grapes": ("Sprites/purple_grapes.png", (0,0,0))
+    "la manzana": ("Sprites/blueberry.png", (0,0,0)),
+    "arandano": ("Sprites/red_apple.png", (0,0,0)),
+    "uva": ("Sprites/purple_grapes.png", (0,0,0))
 	}
 
 french_vehicles ={
