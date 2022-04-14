63f5a7b15c6d1d93bf2716e248a2284daeb461db
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-04-15 00:01:47 +0100

    save

--- src/spanish_stats.py
+++ src/spanish_stats.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-word_stats_spanish = {'entrenar' : '2',
-'la manzana' : '2',
-'el coche' : '2',
-'uva' : '3',
-'projectile' : '3'
-}
-
-spanish_highscore = '0'
-=======
 word_stats_spanish = {
 }
 
 spanish_highscore = ''
 
 spanish_highest_streak = '0'
->>>>>>> 2e1e18d0ba76e4c5ba57c4c623b46dd54164f67d
