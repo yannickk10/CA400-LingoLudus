453ba8bf8064617bc79aabd417f95254f9bb3fb9
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-04-17 06:48:21 +0100

    add new vocab dicts and sounds to dicts values

--- src/vocab.py
+++ src/vocab.py
@@ -1,29 +1,29 @@
 language = 'french'
 spanish_vehicles ={
-    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
-    "el coche": ("Sprites/Car.png", (0,0,0)),
-    "entrenar": ("Sprites/Train.png", (0,0,0)),
-    "la manzana": ("Sprites/blueberry.png", (0,0,0)),
-    "arandano": ("Sprites/red_apple.png", (0,0,0)),
-    "uva": ("Sprites/purple_grapes.png", (0,0,0))
+    "projectile":  ("Sprites/projectile.gif", (0,0,0),),
+    "el coche": ("Sprites/Car.png", (0,0,0),),
+    "entrenar": ("Sprites/Train.png", (0,0,0),),
+    "la manzana": ("Sprites/blueberry.png", (0,0,0),),
+    "arandano": ("Sprites/red_apple.png", (0,0,0),),
+    "uva": ("Sprites/purple_grapes.png", (0,0,0),)
 	}
 
 french_vehicles ={
-    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
-    "la voiturer": ("Sprites/Car.png", (0,0,0)),
-    "le train": ("Sprites/Train.png", (0,0,0)),
-    "Pomme": ("Sprites/blueberry.png", (0,0,0)),
-    "myrtille": ("Sprites/red_apple.png", (0,0,0)),
-    "les raisins": ("Sprites/purple_grapes.png", (0,0,0))
+    "projectile":  ("Sprites/projectile.gif", (0,0,0),),
+    "la voiturer": ("Sprites/Car.png", (0,0,0),),
+    "le train": ("Sprites/Train.png", (0,0,0),),
+    "Pomme": ("Sprites/blueberry.png", (0,0,0),),
+    "myrtille": ("Sprites/red_apple.png", (0,0,0),),
+    "les raisins": ("Sprites/purple_grapes.png", (0,0,0),)
 	}
 
 french_fruits ={
-    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
-    "la voiturer": ("Sprites/Car.png", (0,0,0)),
-    "le train": ("Sprites/Train.png", (0,0,0)),
-    "Pomme": ("Sprites/blueberry.png", (0,0,0)),
-    "myrtille": ("Sprites/red_apple.png", (0,0,0)),
-    "les raisins": ("Sprites/purple_grapes.png", (0,0,0))
+    "projectile":  ("Sprites/projectile.gif", (0,0,0),),
+    "la voiturer": ("Sprites/Car.png", (0,0,0),),
+    "le train": ("Sprites/Train.png", (0,0,0),),
+    "Pomme": ("Sprites/blueberry.png", (0,0,0),),
+    "myrtille": ("Sprites/red_apple.png", (0,0,0),),
+    "les raisins": ("Sprites/purple_grapes.png", (0,0,0),)
 	}
 
 spanish_fruits ={
@@ -35,20 +35,26 @@ spanish_fruits ={
     "uva": ("Sprites/purple_grapes.png", (0,0,0))
 	}
 
-french_numbers ={
-    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
-    "la voiturer": ("Sprites/Car.png", (0,0,0)),
-    "le train": ("Sprites/Train.png", (0,0,0)),
-    "Pomme": ("Sprites/blueberry.png", (0,0,0)),
-    "myrtille": ("Sprites/red_apple.png", (0,0,0)),
-    "les raisins": ("Sprites/purple_grapes.png", (0,0,0))
+spanish_numbers ={
+    "uno":  ("Sprites/one_img.png", (0,0,0),"music/spanish numbers/one.wav"),
+    "dos": ("Sprites/two_img.png", (0,0,0),"music/spanish numbers/two.wav"),
+    "tres": ("Sprites/three_img.png", (0,0,0),"music/spanish numbers/three.wav"),
+    "cuatro": ("Sprites/four_img.png", (0,0,0),"music/spanish numbers/four.wav"),
+    "cinco": ("Sprites/five_img.png", (0,0,0),"music/spanish numbers/five.wav"),
+    "seis": ("Sprites/six_img.png", (0,0,0),"music/spanish numbers/six.wav"),
+    "siete": ("Sprites/seven_img.png", (0,0,0),"music/spanish numbers/seven.wav"),
+    "ocho": ("Sprites/eight_img.png", (0,0,0),"music/spanish numbers/eight.wav"),
+    "nueve": ("Sprites/nine_img.png", (0,0,0),"music/spanish numbers/nine.wav")
 	}
 
-spanish_numbers ={
-    "projectile":  ("Sprites/projectile.gif", (0,0,0)),
-    "la voiturer": ("Sprites/Car.png", (0,0,0)),
-    "le train": ("Sprites/Train.png", (0,0,0)),
-    "Pomme": ("Sprites/blueberry.png", (0,0,0)),
-    "myrtille": ("Sprites/red_apple.png", (0,0,0)),
-    "les raisins": ("Sprites/purple_grapes.png", (0,0,0))
+french_numbers ={
+    "un":  ("Sprites/one_img.png", (0,0,0),),
+    "deux": ("Sprites/two_img.png", (0,0,0),),
+    "trois": ("Sprites/three_img.png", (0,0,0),),
+    "quatre": ("Sprites/four_img.png", (0,0,0),),
+    "cinq": ("Sprites/five_img.png", (0,0,0),),
+    "six": ("Sprites/six_img.png", (0,0,0),),
+    "sept": ("Sprites/seven_img.png", (0,0,0),),
+    "huit": ("Sprites/eight_img.png", (0,0,0),),
+    "nuef": ("Sprites/nine_img.png", (0,0,0),),
 	}
