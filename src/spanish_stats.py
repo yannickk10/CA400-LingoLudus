c78694ecc316321bb8a12035d0a4c053f767faaf
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-04-17 11:33:09 +0100

    stats update

--- src/spanish_stats.py
+++ src/spanish_stats.py
@@ -1,6 +1,15 @@
-word_stats_spanish = {
+word_stats_spanish = {'cuatro' : '1',
+'cinco' : '1',
+'el melon' : '2',
+'arandano' : '1',
+'unos deportivos' : '3',
+'una camiseta' : '4',
+'una sombrero' : '3',
+'un chaqueta' : '2',
+'un pijama' : '2',
+'una corbata' : '1'
 }
 
-spanish_highscore = ''
+spanish_highscore = '1500'
 
-spanish_highest_streak = ''
+spanish_highest_streak = '10'
