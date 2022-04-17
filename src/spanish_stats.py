1717382e619adee23091ba80270e46b8d0cc924c
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-04-17 14:12:52 +0100

    cache

--- src/spanish_stats.py
+++ src/spanish_stats.py
@@ -1,4 +1,4 @@
-word_stats_spanish = {'cuatro' : '1',
+word_stats_spanish = {'cuatro' : '2',
 'cinco' : '1',
 'el melon' : '2',
 'arandano' : '1',
@@ -7,9 +7,13 @@ word_stats_spanish = {'cuatro' : '1',
 'una sombrero' : '3',
 'un chaqueta' : '2',
 'un pijama' : '2',
-'una corbata' : '1'
+'una corbata' : '1',
+'tres' : '1',
+'nueve' : '1',
+'uno' : '1',
+'la fresa' : '1'
 }
 
-spanish_highscore = '1500'
+spanish_highscore = '100'
 
 spanish_highest_streak = '10'
