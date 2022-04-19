68c24d9493e7cd2b389a3a2cca5bcdcddba9f5c4
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-04-19 18:13:46 +0100

    stats updated

--- src/spanish_stats.py
+++ src/spanish_stats.py
@@ -1,5 +1,5 @@
-word_stats_spanish = {'cuatro' : '1',
-'cinco' : '1',
+word_stats_spanish = {'cuatro' : '5',
+'cinco' : '6',
 'el melon' : '2',
 'arandano' : '1',
 'unos deportivos' : '3',
@@ -7,9 +7,16 @@ word_stats_spanish = {'cuatro' : '1',
 'una sombrero' : '3',
 'un chaqueta' : '2',
 'un pijama' : '2',
-'una corbata' : '1'
+'una corbata' : '1',
+'tres' : '3',
+'siete' : '4',
+'ocho' : '3',
+'nueve' : '3',
+'uno' : '2',
+'dos' : '5',
+'seis' : '2'
 }
 
-spanish_highscore = '1500'
+spanish_highscore = '1000'
 
 spanish_highest_streak = '10'
