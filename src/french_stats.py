30633a0e97d81ad30550ec3516570703d4b9339c
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-04-23 00:47:04 +0100

    french stats updated

--- src/french_stats.py
+++ src/french_stats.py
@@ -1,12 +1,16 @@
-word_stats_french = {'un' : '1',
+word_stats_french = {'un' : '2',
 'orange' : '3',
 'les raisins' : '2',
 'banane' : '2',
 'tomate' : '1',
 'melon' : '1',
-'nuef' : '1'
+'nuef' : '1',
+'sept' : '1',
+'six' : '1',
+'huit' : '1',
+'trois' : '1'
 }
 
-french_highscore = '100'
+french_highscore = '500'
 
 french_highest_streak = '7'
