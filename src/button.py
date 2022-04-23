bb280e35c4255549f4dea816c1e4a6660aa11875
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-04-23 17:43:30 +0100

    merged button2 in one file

--- src/button.py
+++ src/button.py
@@ -46,7 +46,7 @@ class Button:
                 if self.pressed == True:
                     self.pressed = False
                     self.elevation_copy = self.orig_elevation
-                    forward_sound = mixer.Sound("music/forward_click.wav")
+                    forward_sound = mixer.Sound("assets/music/forward_click.wav")
                     forward_sound.play()
                     return True
         else:
@@ -84,3 +84,33 @@ class GoBackButton(Button):
             else:
                 self.top_rectangle_color = '#475F77'
                 self.elevation_copy = self.orig_elevation
+
+class Button2():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
