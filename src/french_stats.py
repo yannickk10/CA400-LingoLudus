d82896921629af352de92528c71925eb320a47db
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2022-04-24 02:50:37 +0100

    stat changes

--- src/french_stats.py
+++ src/french_stats.py
@@ -4,7 +4,6 @@ word_stats_french = {'un' : '2',
 'banane' : '2',
 'tomate' : '1',
 'melon' : '1',
-<<<<<<< HEAD
 'nuef' : '3',
 'trois' : '4',
 'sept' : '3',
@@ -21,24 +20,5 @@ word_stats_french = {'un' : '2',
 }
 
 french_highscore = '600'
-=======
-'nuef' : '1',
-'sept' : '1',
-'six' : '5',
-'huit' : '2',
-'trois' : '3',
-'neuf' : '5',
-'quatre' : '2',
-'deux' : '1',
-'un orange' : '2',
-'un melon' : '4',
-'une banane' : '1',
-'une fraise' : '2',
-'un citron' : '1',
-'une pomme' : '1'
-}
-
-french_highscore = '1100'
->>>>>>> d48dc5718d33d24ab15fddb981bd44ee35aed1f9
 
 french_highest_streak = '14'
