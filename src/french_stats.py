2dcadf395ef3f53cbd067f35e73e73c575622f0a
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2022-04-24 18:56:27 +0100

    cache

--- src/french_stats.py
+++ src/french_stats.py
@@ -12,11 +12,19 @@ word_stats_french = {'un' : '2',
 'cinq' : '4',
 'quatre' : '3',
 'huit' : '5',
-'tres' : '2',
+'tres' : '4',
 'dos' : '1',
 'cuatro' : '1',
-'ocho' : '1',
-'uno' : '1'
+'ocho' : '2',
+'uno' : '2',
+'seis' : '7',
+'nueve' : '2',
+'siete' : '1',
+'el tomate' : '1',
+'cinco' : '2',
+'el melon' : '1',
+'el limon' : '1',
+'uva' : '1'
 }
 
 french_highscore = '600'
